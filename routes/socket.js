084906aa764d0984b263e7abdb5cce3e3b5a1848
Jiawei Huang <jhujhuang@gmail.com>
2016-01-16 22:50:40 +0800

    some try on players

--- routes/socket.js
+++ routes/socket.js
@@ -2,10 +2,30 @@
  * Serve content over a socket
  */
 
+function Player(pId) {
+  this.left = 1;
+  this.right = 1;
+  this.pId = pId;
+  this.addLeft = function(n) {
+    this.left = (this.left + n) % 10;
+  };
+  this.addRight = function(n) {
+    this.right = (this.right + n) % 10;
+  };
+}
+
+var counter = 0;
+
 module.exports = function (socket) {
-  socket.emit('send:name', {
-    name: 'Bob'
+  socket.emit('send:data', {
+    left: 1,
+    right: 1,
+    pId: counter
   });
+  var newPlayer = new Player(counter);
+  counter = counter + 1;
+
+  // TODO: DELETE
 
   setInterval(function () {
     socket.emit('send:time', {
